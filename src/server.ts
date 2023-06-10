5fc6cc983c8af5734e0e80770e3069b5df925c98
Eder Queiroz <62737156+Eder-Queiroz@users.noreply.github.com>
2023-06-09 21:48:10 -0300

    has been created migrations
    the connection to the database was made and the tables created

--- src/server.ts
+++ src/server.ts
@@ -14,4 +14,19 @@ app.use(cors());
 
 app.use(router);
 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        // Se for um instancia do tipo error
+        return res.status(400).json({
+            error: err.message
+        })
+    }
+
+    return res.status(500).json({
+        status: "error",
+        message: "internal serve error"
+    })
+
+})
+
 app.listen(PORT, () => console.log(`Server online!\nServidor rodando na porta ${PORT} => http://localhost:${PORT}`))
