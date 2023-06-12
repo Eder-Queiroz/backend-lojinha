a5bd1cf8a5e03ff0087ca35d79ce72eab46dee9c
Eder Queiroz <62737156+Eder-Queiroz@users.noreply.github.com>
2023-06-12 17:21:57 -0300

    a

--- src/controller/ProductController/ProductController.ts
+++ src/controller/ProductController/ProductController.ts
@@ -1,6 +1,5 @@
 import {Request, Response} from 'express';
 import ProductService from '../../Service/Product/ProductService';
-import StockProductService from '../../Service/StockProduct/StockProductService';
 
 export default class ProductController {
 
